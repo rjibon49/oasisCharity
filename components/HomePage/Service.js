f48c9696904dcd7197b83d1b42dedf9af9fcb8e9
Md.  Raihan Hossain Jibon <53583320+rjibon49@users.noreply.github.com>
2023-10-09 01:14:49 +0600

    making custom slider

--- components/HomePage/Service.js
+++ components/HomePage/Service.js
@@ -1,13 +1,21 @@
 import Link from 'next/link';
 import React from 'react';
+import CustomSlider from '../globalComponents/CustomSlider';
+import slides from "../globalComponents/Slides";
 
 const Service = () => {
+    // const slides = ['Slide 1', 'Slide 2', 'Slide 3','Slide 4', 'Slide 5', 'Slide 6'];
+
+    // Adjust autoplayInterval as needed (in milliseconds)
+  const autoplayInterval = 4000;
+
+
     return (
         <div className='colorBg py-5'>
             <div className='container'>
                 <div className='row'>
-                    <div className='col-xxl-6 col-xl-6 col-lg-6'>
-                        <div className='p-4'>
+                    <div className='col-xxl-7 col-xl-7 col-lg-6'>
+                        {/* <div className='p-4'>
                             <div className='serviceBg'>
                                 <div className='serviceBody'>
                                     <div>
@@ -21,9 +29,11 @@ const Service = () => {
                                     </div>
                                 </div>
                             </div>
-                        </div>
+                        </div> */}
+                        <h1>Custom Slider Example</h1>
+                        <CustomSlider slides={slides} autoplayInterval={autoplayInterval} />
                     </div>
-                    <div className='col-xxl-6 col-xl-6 col-lg-6 textCenter'>
+                    <div className='col-xxl-5 col-xl-5 col-lg-6 textCenter'>
                         <div className='serviceWidth'>
                             <h5 className='colorFont'>Service</h5>
                             <h3 className='text40 pb-2'>Project Oasis Provides Personalized Services</h3>
---
+++ components/globalComponents/Slides.js
@@ -0,0 +1,74 @@
+const slides = [
+    (
+      <div key="slide1" className='py-5'>
+        <div className='d-flex justify-content-around flex-wrap'>
+          <div className='col-xxl-6 col-xl-6 col-lg-6'>
+            <div className='p-4'>
+              <div className='serviceBg'>
+                <div className='serviceBody'>
+                  <div>
+                    <h5 className='colorFont'>Mental Health</h5>
+                    <p className='text18'>Ensure that your psychological needs and comfort are addressed with confidentiality.</p>
+                  </div>
+                  <div className='vertical-line'></div>
+                  <div>
+                    <h5 className='colorFont'>Medical</h5>
+                    <p className='text18'>Covers various topics with your doctor.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    ),
+    (
+      <div key="slide2" className='py-5'>
+        <div className='d-flex justify-content-around flex-wrap'>
+          <div className='col-xxl-6 col-xl-6 col-lg-6'>
+            <div className='p-4'>
+              <div className='serviceBg'>
+                <div className='serviceBody'>
+                  <div>
+                    <h5 className='colorFont'>Wellness</h5>
+                    <p className='text18'>This can be from financial to work-life burnout, exercise, religious support, and much more.</p>
+                  </div>
+                  <div className='vertical-line'></div>
+                  <div>
+                    <h5 className='colorFont'>Urgent Care</h5>
+                    <p className='text18'>Care that is in an emergency for our partners in rural countryside.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    ),
+    (
+      <div key="slide3" className='py-5'>
+        <div className='d-flex justify-content-around flex-wrap'>
+          <div className='col-xxl-6 col-xl-6 col-lg-6'>
+            <div className='p-4'>
+              <div className='serviceBg'>
+                <div className='serviceBody'>
+                  <div>
+                    <h5 className='colorFont'>Home Health</h5>
+                    <p className='text18'>From in-patient to out-patient, we bring you the health services to your home.</p>
+                  </div>
+                  <div className='vertical-line'></div>
+                  <div>
+                    <h5 className='colorFont'>Case Management</h5>
+                    <p className='text18'>We treat each patient with personalized care.</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  ];
+  
+  export default slides;
+  
